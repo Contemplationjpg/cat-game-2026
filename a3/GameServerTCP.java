959056c475745f4b8559a8eaccab2727483bfcb6
M. Saechao <55562924+Contemplationjpg@users.noreply.github.com>
2026-04-14 07:32:46 -0700

    added UDP client server functionality

---
+++ a3/GameServerTCP.java
@@ -0,0 +1,110 @@
+package a3;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.UUID;
+import tage.networking.server.GameConnectionServer;
+import tage.networking.server.IClientInfo;
+
+public class GameServerTCP extends GameConnectionServer<UUID> {
+
+    public GameServerTCP(int localPort) throws IOException {
+        super(localPort, ProtocolType.TCP);
+        System.out.println("TCP Server: " + localPort);
+    }
+
+    @Override
+    public void processPacket(Object o, InetAddress senderIP, int sndPort) {
+        String message = (String) o;
+        System.out.println("PACKET RECEIVED FROM CLIENT: " + message);
+        String[] msgTokens = message.split(",");
+        if (msgTokens.length > 0) {
+            // case where server receives a JOIN message
+            // format: join,localid
+            if (msgTokens[0].compareTo("join") == 0) {
+                try {
+                    IClientInfo ci;
+                    ci = getServerSocket().createClientInfo(senderIP, sndPort);
+                    UUID clientID = UUID.fromString(msgTokens[1]);
+                    addClient(ci, clientID);
+                    sendJoinedMessage(clientID, true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // case where server receives a CREATE message
+        // format: create,localid,x,y,z
+        if (msgTokens[0].compareTo("create") == 0) {
+            UUID clientID = UUID.fromString(msgTokens[1]);
+            String[] pos = {msgTokens[2], msgTokens[3], msgTokens[4]};
+            sendCreateMessages(clientID, pos);
+            sendWantsDetailsMessages(clientID);
+        }
+
+        // case where server receives a BYE message
+        // format: bye,localid
+        if (msgTokens[0].compareTo("bye") == 0) {
+            UUID clientID = UUID.fromString(msgTokens[1]);
+            sendByeMessages(clientID);
+            removeClient(clientID);
+        }
+
+        // case where server receives a DETAILS-FOR message
+        if (msgTokens[0].compareTo("dsfr") == 0) {
+        }
+
+        // case where server receives a MOVE message
+        if (msgTokens[0].compareTo("move") == 0) {
+            System.out.print("MOVE RECEIVED FROM " + senderIP + ": " + msgTokens[1]);
+        }
+
+    }
+
+    public void sendJoinedMessage(UUID clientID, boolean success) {
+        // format: join, success or join, failure
+        System.out.println("sendJoinedMessage(" + clientID + ", " + success + ")");
+        try {
+            String message = "join,";
+            if (success) {
+                message += "success";
+            } else {
+                message += "failure";
+            }
+            sendPacket(message, clientID);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendCreateMessages(UUID clientID, String[] position) {
+        // format: create, remoteId, x, y, z
+        try {
+            String message = "create," + clientID.toString();
+            message += "," + position[0];
+            message += "," + position[1];
+            message += "," + position[2];
+            forwardPacketToAll(message, clientID);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sndDetailsMsg(UUID clientID, UUID remoteId, String[] position) {
+        System.out.println(clientID + ", " + remoteId + ", " + position);
+    }
+
+    public void sendWantsDetailsMessages(UUID clientID) {
+        System.out.println(clientID + " reqesting details");
+    }
+
+    public void sendMoveMessages(UUID clientID, String[] position) {
+        System.out.println(clientID + ", " + position);
+    }
+
+    public void sendByeMessages(UUID clientID) {
+        System.out.println(clientID + " Bye");
+    }
+
+}
---
+++ a3/ProtocolClient.java
@@ -0,0 +1,104 @@
+package a3;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.UUID;
+import tage.*;
+import tage.GhostAvatar.*;
+import tage.networking.client.GameConnectionClient;
+import org.joml.Vector3f;
+// import org.joml.Vector3;
+
+public class ProtocolClient extends GameConnectionClient {
+
+    private MyGame game;
+    private UUID id;
+    private GhostManager ghostManager;
+
+    public ProtocolClient(InetAddress remAddr, int remPort,
+            ProtocolType pType, MyGame game) throws IOException {
+        super(remAddr, remPort, pType);
+        this.game = game;
+        this.id = UUID.randomUUID();
+        ghostManager = game.getGhostManager();
+    }
+
+    @Override
+    protected void processPacket(Object msg) {
+        System.out.println("RECEIVED PACKET: " + msg);
+        String strMessage = (String) msg;
+        String[] msgTokens = strMessage.split(",");
+        if (msgTokens.length > 0) {
+            if (msgTokens[0].compareTo("join") == 0) // receive "join"
+            { // format: join, success or join, failure
+                if (msgTokens[1].compareTo("success") == 0) {
+                    game.setIsConnected(true);
+                    sendCreateMessage(game.getPlayerPosition());
+                }
+                if (msgTokens[1].compareTo("failure") == 0) {
+                    game.setIsConnected(false);
+                }
+            }
+            if (msgTokens[0].compareTo("bye") == 0) // receive "bye"
+            { // format: bye, remoteId
+                UUID ghostID = UUID.fromString(msgTokens[1]);
+                ghostManager.removeGhostAvatar(ghostID);
+            }
+            if ((msgTokens[0].compareTo("dsfr") == 0) // receive "dsfr"
+                    || (msgTokens[0].compareTo("create") == 0)) { // format: create, remoteId, x,y,z or dsfr, remoteId, x,y,z
+                UUID ghostID = UUID.fromString(msgTokens[1]);
+                Vector3f ghostPosition = new Vector3f(
+                        Float.parseFloat(msgTokens[2]),
+                        Float.parseFloat(msgTokens[3]),
+                        Float.parseFloat(msgTokens[4]));
+                try {
+                    ghostManager.createGhost(ghostID, ghostPosition);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("error creating ghost avatar");
+                }
+            }
+            if (msgTokens[0].compareTo("wsds") == 0) // rec. "wants…"
+            {
+            }
+            if (msgTokens[0].compareTo("move") == 0) // rec. "move..."
+            {
+            }
+        }
+    }
+
+    public void sendJoinMessage() // format: join, localId
+    {
+        try {
+            sendPacket(new String("join," + id.toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendCreateMessage(Vector3f pos) { // format: (create, localId, x,y,z)
+        try {
+            String message = new String("create," + id.toString());
+            message += "," + pos.x + "," + pos.y + "," + pos.z;
+            sendPacket(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendByeMessage() {
+
+    }
+
+    public void sendDetailsForMessage(UUID remId, Vector3f pos) {
+
+    }
+
+    public void sendMoveMessage(Vector3f pos) {
+        try {
+            sendPacket(new String("move," + pos));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
