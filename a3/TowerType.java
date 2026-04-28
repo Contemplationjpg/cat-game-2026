73979dea3ae68d2e084af7efcd27a5f4ede153ec
M. Saechao <55562924+Contemplationjpg@users.noreply.github.com>
2026-04-28 16:03:12 -0700

    Type TowerType stores name, model, texture

---
+++ a3/TowerType.java
@@ -0,0 +1,113 @@
+package a3;
+
+import tage.ObjShape;
+import tage.TextureImage;
+import tage.shapes.ImportedModel;
+
+public class TowerType {
+
+    private String name;
+    private ObjShape model;
+    private TextureImage texture;
+
+    public TowerType(String n) {
+        name = n;
+        String objFileStr = name + ".obj";
+        try {
+            model = new ImportedModel(objFileStr);
+        } catch (Exception e) {
+            System.err.print("unable to find model for " + n);
+        }
+        String texFileStr = name + ".jpg";
+        try {
+            texture = new TextureImage(texFileStr);
+        } catch (Exception e) {
+            System.err.print("unable to find texture " + texFileStr);
+        }
+    }
+
+    public TowerType(String n, ObjShape m) {
+        name = n;
+        model = m;
+    }
+
+    public TowerType(String n, String mStr) {
+        name = n;
+        String objFileStr = mStr + ".obj";
+        try {
+            model = new ImportedModel(objFileStr);
+        } catch (Exception e) {
+            System.err.print("unable to find model for " + mStr);
+        }
+    }
+
+    public TowerType(String n, ObjShape m, TextureImage t) {
+        name = n;
+        model = m;
+        texture = t;
+    }
+
+    public TowerType(String n, String mStr, TextureImage t) {
+        name = n;
+        texture = t;
+        String objFileStr = mStr + ".obj";
+        try {
+            model = new ImportedModel(objFileStr);
+        } catch (Exception e) {
+            System.err.print("unable to find model for " + mStr);
+        }
+    }
+
+    public TowerType(String n, ObjShape m, String tStr) {
+        name = n;
+        model = m;
+        String texFileStr = tStr + ".jpg";
+        try {
+            texture = new TextureImage(texFileStr);
+        } catch (Exception e) {
+            System.err.print("unable to find texture " + texFileStr);
+        }
+    }
+
+    public TowerType(String n, String mStr, String tStr) {
+        name = n;
+        String texFileStr = tStr + ".jpg";
+        try {
+            texture = new TextureImage(texFileStr);
+        } catch (Exception e) {
+            System.err.print("unable to find texture " + texFileStr);
+        }
+
+        String objFileStr = mStr + ".obj";
+        try {
+            model = new ImportedModel(objFileStr);
+        } catch (Exception e) {
+            System.err.print("unable to find model " + objFileStr);
+        }
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setModel(ObjShape m) {
+        model = m;
+    }
+
+    public ObjShape getModel() {
+        return model;
+    }
+
+    public void setTexture(TextureImage t) {
+        texture = t;
+    }
+
+    public TextureImage getTexture() {
+        return texture;
+    }
+
+}
